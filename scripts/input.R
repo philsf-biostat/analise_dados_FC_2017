3cf87b522dab20d1a8666b8077740b49002c058d
Felipe Figueiredo <philsf79@gmail.com>
2017-04-19 20:40:00 -0300

    novos dados padronizados

--- scripts/input.R
+++ scripts/input.R
@@ -4,7 +4,24 @@ library(data.table)
 DF <- read_excel("dataset/brutos/Tabela Dados.xlsx", 2, skip = 1)
 DF <- data.table(DF)
 
-DF$NÍVES <- factor(DF$NÍVES)
+DF <- DF[,.(PRONTUÁRIO,
+      GENERO,
+      IDADE,
+      `CERVICAL ALTA`,
+      PLACA,
+      NIVEIS,
+      CORPECTOMIAS,
+      MIELOPATIA,
+      `DURAÇÃO CIR`,
+      `FRATURA / SEQUELA`,
+      PESO,
+      ALTURA,
+      IMC,
+      DISFONIA,
+      DISFAGIA
+      )]
+
+DF$NIVEIS <- factor(DF$NIVEIS)
 DF$CORPECTOMIAS <- factor(DF$CORPECTOMIAS)
 DF$DISFAGIA <- factor(DF$DISFAGIA)
 DF$DISFONIA <- factor(DF$DISFONIA)
@@ -12,3 +29,5 @@ DF$`FRATURA / SEQUELA` <- factor(DF$`FRATURA / SEQUELA`)
 DF$MIELOPATIA <- factor(DF$MIELOPATIA)
 DF$`CERVICAL ALTA` <- factor(DF$`CERVICAL ALTA`)
 DF$PLACA <- factor(DF$PLACA)
+DF$GENERO <- factor(DF$GENERO)
+DF$PRONTUÁRIO <- factor(DF$PRONTUÁRIO)
