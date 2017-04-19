91d9628794fa25b95894347c23c6b8e4e646c043
Felipe Figueiredo <philsf79@gmail.com>
2017-04-19 19:22:14 -0300

    gerenciamento dos fatores

--- scripts/input.R
+++ scripts/input.R
@@ -3,3 +3,12 @@ library(data.table)
 
 DF <- read_excel("dataset/brutos/Tabela Dados.xlsx", 2, skip = 1)
 DF <- data.table(DF)
+
+DF$NÍVES <- factor(DF$NÍVES)
+DF$CORPECTOMIAS <- factor(DF$CORPECTOMIAS)
+DF$DISFAGIA <- factor(DF$DISFAGIA)
+DF$DISFONIA <- factor(DF$DISFONIA)
+DF$`FRATURA / SEQUELA` <- factor(DF$`FRATURA / SEQUELA`)
+DF$MIELOPATIA <- factor(DF$MIELOPATIA)
+DF$`CERVICAL ALTA` <- factor(DF$`CERVICAL ALTA`)
+DF$PLACA <- factor(DF$PLACA)
