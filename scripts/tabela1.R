aad3e6b93dafdd033dd89a5ffd2a84aea518d583
Felipe Figueiredo <philsf79@gmail.com>
2017-04-19 21:19:42 -0300

    tabelas usando teste exato de Fisher

--- scripts/tabela1.R
+++ scripts/tabela1.R
@@ -1,3 +1,3 @@
 library(tableone)
-CreateTableOne(data = DF, vars = c("IDADE", "IMC", "NIVEIS"), strata = "DISFAGIA")
-CreateTableOne(data = DF, vars = c("IDADE", "IMC", "NIVEIS"), strata = "DISFONIA")
+print(CreateTableOne(data = DF, vars = c("IDADE", "IMC", "NIVEIS"), strata = "DISFAGIA"), exact = T)
+print(CreateTableOne(data = DF, vars = c("IDADE", "IMC", "NIVEIS"), strata = "DISFONIA"), exact = T)
