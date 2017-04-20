08f80f9964b49b19a49e9629125bb6eaf12d1d8b
Felipe Figueiredo <philsf79@gmail.com>
2017-04-19 21:48:09 -0300

    modelos de regressão atualizados para novas variáveis, e modelos saturados incluídos

--- scripts/reglog.R
+++ scripts/reglog.R
@@ -1,13 +1,13 @@
 
-summary(with(DF, glm(DISFONIA ~ `NÍVES` + CORPECTOMIAS, family = binomial)))
-summary(with(DF, glm(DISFONIA ~ `NÍVES` + CORPECTOMIAS, family = binomial)))
+summary(with(DF, glm(DISFONIA ~ NIVEIS + CORPECTOMIAS, family = binomial)))
+summary(with(DF, glm(DISFONIA ~ NIVEIS:CORPECTOMIAS, family = binomial)))
 
-summary(with(DF, glm(DISFAGIA ~ `NÍVES` + CORPECTOMIAS, family = binomial)))
-summary(with(DF, glm(DISFAGIA ~ `NÍVES` + CORPECTOMIAS, family = binomial)))
+summary(with(DF, glm(DISFAGIA ~ NIVEIS + CORPECTOMIAS, family = binomial)))
+summary(with(DF, glm(DISFAGIA ~ NIVEIS:CORPECTOMIAS, family = binomial)))
 
 
-summary(with(DF, glm(DISFONIA ~ `NÍVES` , family = binomial)))
+summary(with(DF, glm(DISFONIA ~ NIVEIS , family = binomial)))
 summary(with(DF, glm(DISFONIA ~ CORPECTOMIAS , family = binomial)))
 
-summary(with(DF, glm(DISFAGIA ~ `NÍVES` , family = binomial)))
+summary(with(DF, glm(DISFAGIA ~ NIVEIS , family = binomial)))
 summary(with(DF, glm(DISFAGIA ~ CORPECTOMIAS , family = binomial)))
