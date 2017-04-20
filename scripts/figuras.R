b0ee662e3f40ca859f4bc3a595fd0fcef82ac702
Felipe Figueiredo <philsf79@gmail.com>
2017-04-19 21:38:24 -0300

    gráfico de disfagia incluido, e gráficos com títulos

--- scripts/figuras.R
+++ scripts/figuras.R
@@ -1,1 +1,2 @@
-with(DF, barplot(table(NIVEIS, DISFONIA), beside = T, legend.text = T))
+with(DF, barplot(table(NIVEIS, DISFONIA), beside = T, legend.text = T, main = "DISFONIA"))
+with(DF, barplot(table(NIVEIS, DISFAGIA), beside = T, legend.text = T, main = "DISFAGIA"))
