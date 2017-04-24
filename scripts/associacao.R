a2d6fe6e90e7644f4f553f0f4f3bdf27f144d297
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-04-24 17:26:10 -0300

    testes de fisher / frat renomeada

--- scripts/associacao.R
+++ scripts/associacao.R
@@ -4,12 +4,44 @@ ob.disfon <- with(DF, table(OBESO, DISFONIA))
 cir.disfon <- with(DF, table(LONGA, DISFONIA))
 corpec.disfon <- with(DF, table(CORPECTOMIAS, DISFONIA))
 mielo.disfon <- with(DF, table(MIELOPATIA, DISFONIA))
-fratura.disfon <- with(DF, table(`FRATURA / SEQUELA`, DISFONIA))
+frat.disfon <- with(DF, table(`FRATURA / SEQUELA`, DISFONIA))
 gen.disfon <- with(DF, table(GENERO, DISFONIA))
 niveis.disfon <- with(DF, table(NIVEIS, DISFONIA))
 placa.disfon <- with(DF, table(PLACA, DISFONIA))
 cervic.disfon <- with(DF, table(`CERVICAL ALTA`, DISFONIA))
 # .disfon <- with(DF, table(, DISFONIA))
+
+# Fisher test
+ob.disfon.fisher <- fisher.test(ob.disfon)
+cir.disfon.fisher <- fisher.test(cir.disfon)
+corpec.disfon.fisher <- fisher.test(corpec.disfon)
+mielo.disfon.fisher <- fisher.test(mielo.disfon)
+frat.disfon.fisher <- fisher.test(frat.disfon)
+gen.disfon.fisher <- fisher.test(gen.disfon)
+niveis.disfon.fisher <- fisher.test(niveis.disfon)
+placa.disfon.fisher <- fisher.test(placa.disfon)
+cervic.disfon.fisher <- fisher.test(cervic.disfon)
+# ob.disfon.fisher <- fisher.test(ob.disfon)
+
+# OR
+ob.disfon.or <- ob.disfon.fisher$estimate
+ob.disfon.or.CI <- ob.disfon.fisher$conf.int
+cir.disfon.or <- cir.disfon.fisher$estimate
+cir.disfon.or.CI <- cir.disfon.fisher$conf.int
+corpec.disfon.or <- corpec.disfon.fisher$estimate
+corpec.disfon.or.CI <- corpec.disfon.fisher$conf.int
+mielo.disfon.or <- mielo.disfon.fisher$estimate
+mielo.disfon.or.CI <- mielo.disfon.fisher$conf.int
+frat.disfon.or <- frat.disfon.fisher$estimate
+frat.disfon.or.CI <- frat.disfon.fisher$conf.int
+ob.disfon.or <- ob.disfon.fisher$estimate
+ob.disfon.or.CI <- ob.disfon.fisher$conf.int
+ob.disfon.or <- ob.disfon.fisher$estimate
+ob.disfon.or.CI <- ob.disfon.fisher$conf.int
+ob.disfon.or <- ob.disfon.fisher$estimate
+ob.disfon.or.CI <- ob.disfon.fisher$conf.int
+
+
 
 # DISFAGIA ####
 # tabelas
@@ -17,7 +49,7 @@ ob.disfag <- with(DF, table(OBESO, DISFAGIA))
 cir.disfag <- with(DF, table(LONGA, DISFAGIA))
 corpec.disfag <- with(DF, table(CORPECTOMIAS, DISFAGIA))
 mielo.disfag <- with(DF, table(MIELOPATIA, DISFAGIA))
-fratura.disfag <- with(DF, table(`FRATURA / SEQUELA`, DISFAGIA))
+frat.disfag <- with(DF, table(`FRATURA / SEQUELA`, DISFAGIA))
 gen.disfag <- with(DF, table(GENERO, DISFAGIA))
 niveis.disfag <- with(DF, table(NIVEIS, DISFAGIA))
 placa.disfag <- with(DF, table(PLACA, DISFAGIA))
