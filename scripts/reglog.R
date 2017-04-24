ff025db247c91f573c67c557496c3f2ca1942a8d
Felipe Figueiredo <philsf79@gmail.com>
2017-04-23 23:39:43 -0300

    modelos de regressão para DURACAO (minutos) e DUR.cat (horas completas)

--- scripts/reglog.R
+++ scripts/reglog.R
@@ -11,3 +11,9 @@ summary(with(DF, glm(DISFONIA ~ CORPECTOMIAS , family = binomial)))
 
 summary(with(DF, glm(DISFAGIA ~ NIVEIS , family = binomial)))
 summary(with(DF, glm(DISFAGIA ~ CORPECTOMIAS , family = binomial)))
+
+summary(with(DF, glm(DISFONIA ~ DURACAO, family = binomial))) # minutos
+summary(with(DF, glm(DISFONIA ~ DUR.cat, family = binomial))) # horas
+
+summary(with(DF, glm(DISFAGIA ~ DURACAO, family = binomial))) # minutos
+summary(with(DF, glm(DISFAGIA ~ DUR.cat, family = binomial))) # horas
