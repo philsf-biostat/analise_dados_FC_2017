6fe9b636b9bd693e933bcf0f5cbd7195ab9a7993
Felipe Figueiredo <philsf79@gmail.com>
2017-04-23 22:02:48 -0300

    DURACAO incluído nas tabelas

--- scripts/tabela1.R
+++ scripts/tabela1.R
@@ -1,3 +1,3 @@
 library(tableone)
-print(CreateTableOne(data = DF, vars = c("IDADE", "IMC", "NIVEIS"), strata = "DISFAGIA"), exact = T)
-print(CreateTableOne(data = DF, vars = c("IDADE", "IMC", "NIVEIS"), strata = "DISFONIA"), exact = T)
+print(CreateTableOne(data = DF, vars = c("IDADE", "IMC", "DURACAO", "NIVEIS"), strata = "DISFAGIA"), exact = T)
+print(CreateTableOne(data = DF, vars = c("IDADE", "IMC", "DURACAO", "NIVEIS"), strata = "DISFONIA"), exact = T)
